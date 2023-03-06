15bfcc14fc3bf97a7e3b6f8d66b6a22a3fd462df
이재후 <jaehoo@ijaehuui-MacBookPro.local>
2023-03-06 20:24:47 +0900

    Initial commit

---
+++ src/main/java/com/jadoobox/boxV1/BoxV1Application.java
@@ -0,0 +1,13 @@
+package com.jadoobox.boxV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jadoobox/boxV1/BoxV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jadoobox.boxV1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
